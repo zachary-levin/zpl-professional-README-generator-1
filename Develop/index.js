7a18a6f6a0c37e576d1a010d67d1f4fa2d3be854
zachary-levin <60529010+zachary-levin@users.noreply.github.com>
2022-03-02 18:58:49 -0600

    added prompt requiring user input that asks user to enter title and description of project with five follow-up questions

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,105 @@
-// TODO: Include packages needed for this application
+// 1. TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown');
 
-// TODO: Create an array of questions for user input
-const questions = [];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// 2. TODO: Create an array of questions for user input
 
-// TODO: Create a function to initialize app
-function init() {}
+const promptQuestions = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'title',
+            message: 'What is the title of your project? (Required)',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter the name of the project!');
+                    return false;
+                }
+            }
+        },
 
-// Function call to initialize app
+// 2.5(?) TODO: Create a question asking user to enter a short description
+
+        {
+            type: 'input',
+            name: 'motivation',
+            message: 'What was your motivation? (Required)',
+            validate: motivationInput => {
+                if (motivationInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your motivation reason!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'buildReason',
+            message: 'Why did you build this project? (Note: the answer is not "Because it was a homework assignment.") (Required)',
+            validate: buildReasonInput => {
+                if (buildReasonInput) {
+                    return true;
+                } else {
+                    console.log('Please enter the reason for why the project was built!')
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'problem',
+            message: 'What problem does it solve? (Required)',
+            validate: problemInput => {
+                if (problemInput) {
+                    return true;
+                } else {
+                    console.log('Please enter what problem the project solves!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'learn',
+            message: 'What did you learn? (Required)',
+            validate: learnInput => {
+                if (learnInput) {
+                    return true;
+                } else {
+                    console.log('Please enter what you learned from this project!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'standoutReason',
+            message: 'What makes your project stand out? (Required)',
+            validate: standoutReasonInput => {
+                if (standoutReasonInput) {
+                    return true;
+                } else {
+                    console.log('Please enter what makes your project stand out!');
+                    return false;
+                }
+            }
+        }
+    ])
+}
+
+promptQuestions();
+
+// 7. TODO: Create a function to write README file
+function writeToFile(fileName, data) { }
+
+// 8. TODO: Create a function to initialize app
+function init() { }
+
+// 9. Function call to initialize app
 init();
+
+// Work on index.js file first, then do generateMarkdown.js file second.
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,16 +1,16 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
+// 3. TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {}
 
-// TODO: Create a function that returns the license link
+// 4. TODO: Create a function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {}
 
-// TODO: Create a function that returns the license section of README
+// 5. TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {}
 
-// TODO: Create a function to generate markdown for README
+// 6. TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
 
